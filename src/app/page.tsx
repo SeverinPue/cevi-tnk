c0b6ffe0ba6fc0f25b6674079f7348887f5def55
Severin <severin.puentener@gmx.ch>
2024-04-18 16:46:47 +0200

    trying to import pdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import Footer from "./Layout/Footer";
 import Header from "./Layout/Header";
+import pdf from 'src\app\GLK1Programminhalt.pdf'
 
 export default function Home() {
+
+
+
   return (
     <>
-      <Header></Header>
       <main>
 
-
+        <embed src="" height="500" width="1000" />
 
       </main>
 
-      <Footer></Footer>
     </>
   );
 }
